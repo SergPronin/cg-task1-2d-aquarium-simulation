70f86ba3f5d0ecce9e60bc755e8f8087fc8c3ee4
Sergey Pronin <sergeypronin.dev@gmail.com>
2025-09-17 17:11:06 +0300

    Initial commit

---
+++ src/main/java/ru/vsu/cs/cg/pronin_s_v/task1_primitives/MainWindow.java
@@ -0,0 +1,12 @@
+package ru.vsu.cs.cg.pronin_s_v.task1_primitives;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public static void main(String[] args) {
+        MainWindow mw = new MainWindow();
+        mw.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        mw.setSize(800, 600);
+        mw.setVisible(true);
+    }
+}
